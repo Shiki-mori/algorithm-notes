54f91d3dfd4f6ce2109d5db7b5c93163ff830b9c
Shiki-mori <shiki_mori@qq.com>
2026-08-12 22:28:37 +0800

    递归求数组最大值

---
+++ algorithm/src/main/java/com/phrolova/algorithm/recursion/RecursiveMax.java
@@ -0,0 +1,17 @@
+package com.phrolova.algorithm.recursion;
+
+public class RecursiveMax {
+    public static int recursiveMax(int[] arr) {
+        return process(arr, 0, arr.length - 1);
+    }
+
+    public static int process(int[] arr, int L, int R) {
+        if (L == R) {
+            return arr[L];
+        }
+        int mid = L + (R - L) >> 1;
+        int leftMax = process(arr, L, mid);
+        int rightMax = process(arr, mid + 1, R);
+        return Math.max(leftMax, rightMax);
+    }
+}
